4a413c741462f302555121cfaa8ab86f09acccfa
Imran <aakhtarimran92@gmail.com>
2022-09-11 16:46:46 +0530

    install material UI and icon and design Header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { CssBaseline, AppBar, Typography } from "@mui/material";
+import ToolBar from "@mui/material/Toolbar";
+import AdjustIcon from "@mui/icons-material/Adjust";
 
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <CssBaseline />
+      <AppBar postion="relative">
+        <ToolBar>
+          <AdjustIcon sx={{ mr: 1 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Photo Album
+          </Typography>
+        </ToolBar>
+      </AppBar>
+    </>
+  );
 };
 
 export default App;
