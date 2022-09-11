cd5aebea66c6a969389e25cd0aa8772001f0f084
Imran <aakhtarimran92@gmail.com>
2022-09-11 18:31:40 +0530

    header section added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { CssBaseline, AppBar, Typography } from "@mui/material";
+import {
+  CssBaseline,
+  AppBar,
+  Typography,
+  Box,
+  Container,
+  Stack,
+  Button,
+} from "@mui/material";
 import ToolBar from "@mui/material/Toolbar";
 import AdjustIcon from "@mui/icons-material/Adjust";
 
@@ -15,6 +23,46 @@ const App = () => {
           </Typography>
         </ToolBar>
       </AppBar>
+      <main>
+        <Box
+          sx={{
+            bgcolor: "background.paper",
+            pt: 10,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="text.secondary"
+              paragraph
+            >
+              Something short and leading about the collection below—its
+              contents, the creator, etc. Make it short and sweet, but not too
+              short so folks don&apos;t simply skip over it entirely.
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={4}
+              justifyContent="center"
+            >
+              <Button variant="contained">Main call to action</Button>
+              <Button varaint="outlined">Secondary action</Button>
+            </Stack>
+          </Container>
+        </Box>
+      </main>
     </>
   );
 };
