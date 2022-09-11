5206a82068b8bc43f1825297c9f50a32ea771ed1
Imran <aakhtarimran92@gmail.com>
2022-09-11 19:28:58 +0530

    photo album section updated

--- src/App.js
+++ src/App.js
@@ -7,9 +7,16 @@ import {
   Container,
   Stack,
   Button,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
 } from "@mui/material";
 import ToolBar from "@mui/material/Toolbar";
 import AdjustIcon from "@mui/icons-material/Adjust";
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const App = () => {
   return (
@@ -62,6 +69,40 @@ const App = () => {
             </Stack>
           </Container>
         </Box>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    image="https://images.pexels.com/photos/12004322/pexels-photo-12004322.jpeg"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe
+                      the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="smalll">View</Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
       </main>
     </>
   );
